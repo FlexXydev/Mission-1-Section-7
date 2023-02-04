b6a45586fcebd17b71a07ca2231cf1c802ac3645
Bexeos <lolmdrlili94360@gmail.com>
2023-02-04 14:49:24 +0100

    v1.0

---
+++ Js/main.js
@@ -0,0 +1,94 @@
+let panels = {
+    question: null,
+    answer: null,
+    end: null, 
+}
+let countries = [];
+let question = {};
+let questionNumber = 1;
+let questionTotal = 10;
+let goodAnswers = 0;
+
+
+const init = async () => {
+    panels.question = document.querySelector('#question-panel'); 
+    panels.answer = document.querySelector('#answer-panel'); 
+    panels.end = document.querySelector('#end-panel');
+    
+    // Get all countries
+    const response = await fetch('https://restcountries.com/v2/all')
+    countries = await response.json()
+
+    // Add events listeners
+        // Click on answer
+    panels.question.querySelector('ul').addEventListener('click', ({ target }) => {
+       if (target.matches('li')) {
+        const userAnswer = target.innerHTML;
+        checkAnswer(userAnswer)
+       }
+    });
+        // Click on next question
+    panels.answer.querySelector('button').addEventListener('click', () => {
+        if (questionNumber <= questionTotal) {
+         pickQuestion();
+        switchPanel('question');
+        } else {
+            panels.end.querySelector('p').innerHTML = `Your score is: ${goodAnswers} / ${questionTotal}`
+            switchPanel('end')
+        }
+
+    });
+
+    pickQuestion();
+};
+
+    const pickQuestion = () => {
+        question = generateQuestion(countries);
+
+        // Display Question
+        panels.question.querySelector('img').setAttribute('src', question.flag);
+        panels.question.querySelector('small').innerHTML = `${questionNumber} / ${questionTotal}`
+        const possibilitiesHtml = question.possibilities.map((possibility) => {
+            return `<li>${possibility}</li>`;
+        });
+        panels.question.querySelector('ul').innerHTML = possibilitiesHtml.join('')
+}
+
+// Switch Panels
+const switchPanel = (panel) => {
+    switch (panel) {
+        case 'answer': 
+            panels.answer.style.display = 'block'
+            panels.question.style.display = 'none'
+            panels.end.style.display = 'none'
+            break;
+        case 'question': 
+            panels.question.style.display = 'block'
+            panels.answer.style.display = 'none'
+            panels.end.style.display = 'none'
+            break;
+        default:
+            panels.end.style.display = 'block'
+            panels.answer.style.display = 'none'
+            panels.answer.style.display = 'none'
+            break;
+    }
+}
+
+// checkAnswer
+const checkAnswer = (userAnswer) => {
+    console.log('userAnswer', userAnswer);
+    if (userAnswer === question.answer) {
+        panels.answer.querySelector('h2').innerHTML = 'Good Answer !';
+        panels.answer.querySelector('p').innerHTML = '';
+    goodAnswers++;
+    } else {
+        panels.answer.querySelector('h2').innerHTML = 'Wrong Answer !';
+        panels.answer.querySelector('p').innerHTML = `The answer was: ${question.answer} !`;
+    }
+    questionNumber++;
+    switchPanel('answer');
+}
+
+
+window.onload = init; 
---
+++ README.md
@@ -0,0 +1,30 @@
+# Guess The Flag
+
+# Demo :
+If you wont to try the game, here is the demo : http://saturnguesstheflag.000webhostapp.com/
+
+# Preview :
+Here is a preview of the game :
+<img src="https://cdn.discordapp.com/attachments/1047551942710276107/1071422548639760434/image.png"/>
+
+# What is Section 7 ?
+Section 7 is a French developer community discord server. In this server their mission like this game.
+
+## Invite to Section 7 server :
+https://discord.gg/Ms78rJFa5d 
+
+# Instalation :
+## Step 1 :
+
+```
+git clone https://github.com/FlexXydev/Mission-1-Section-7
+```
+
+## Step 2 :
+
+```
+cd Mission-1-Section-7
+```
+
+## Step 3 :
+Open the index.html file
